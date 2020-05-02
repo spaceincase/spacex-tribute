adbf4b7eaa8bbf93d858f3843f14c79c777b6540
spaceincase <bradlypetrsn1991@gmail.com>
2020-05-02 13:53:05 -0500

    Changed cost per launch to millions

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -16,9 +16,8 @@ Array.from(imgs).forEach(img => {
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementById("close");
+let span = document.getElementById("close");
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.addEventListener('click', () => {
   modal.style.display = "none";
-}
+})
